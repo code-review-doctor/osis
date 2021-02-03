59948ca95cf305bcbf05ff74c2d9be0442e6f94e
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2021-02-03 11:45:23 +0100

    OSIS-5526 : Adapt query and fix generated title

--- education_group/migrations/0018_default_titles_and_credits.py
+++ education_group/migrations/0018_default_titles_and_credits.py
@@ -1,4 +1,5 @@
 from django.db import migrations
+from django.utils import timezone
 
 from base.models.enums.education_group_types import GroupType
 from base.models.group_element_year import GroupElementYear
@@ -7,7 +8,7 @@ from education_group.models.group_year import GroupYear
 
 def add_default_credits_to_minor_list_choices(apps, schema_editor):
     geys = GroupElementYear.objects.filter(
-        parent_element__group_year__academic_year__year__gte=2019,
+        parent_element__group_year__academic_year__year__gt=2019,
         parent_element__group_year__education_group_type__name=GroupType.MINOR_LIST_CHOICE.name,
         child_element__isnull=False
     ).exclude(
@@ -17,13 +18,14 @@ def add_default_credits_to_minor_list_choices(apps, schema_editor):
     for gey in geys:
         group_year = gey.parent_element.group_year
         group_year.credits = 30
+        group_year.changed = timezone.now()
         minor_list_choices.append(group_year)
-    GroupYear.objects.bulk_update(minor_list_choices, ['credits'], batch_size=1000)
+    GroupYear.objects.bulk_update(minor_list_choices, ['credits', 'changed'], batch_size=1000)
 
 
 def add_default_titles_to_minor_option_list_choice_and_complementary_module(apps, schema_editor):
     groups = GroupYear.objects.filter(
-        academic_year__year__gte=2019,
+        academic_year__year__gt=2019,
         education_group_type__name__in=[
             GroupType.COMPLEMENTARY_MODULE.name,
             GroupType.MINOR_LIST_CHOICE.name,
@@ -34,17 +36,20 @@ def add_default_titles_to_minor_option_list_choice_and_complementary_module(apps
     for mlc in minor_list_choices:
         mlc.title_fr = 'Mineure ou approfondissement'
         mlc.title_en = 'Minor or additional module'
+        mlc.changed = timezone.now()
     option_list_choices = groups.filter(education_group_type__name=GroupType.OPTION_LIST_CHOICE.name)
     for olc in option_list_choices:
         olc.title_fr = 'Liste des options'
         olc.title_en = 'List of electives'
+        olc.changed = timezone.now()
     complementary_modules = groups.filter(education_group_type__name=GroupType.COMPLEMENTARY_MODULE.name)
     for cm in complementary_modules:
         cm.title_fr = 'Enseignements supplémentaires (module complémentaire) au master'
         cm.title_en = 'Additional courses (preparatory module) to the master'
+        cm.changed = timezone.now()
     GroupYear.objects.bulk_update(
         list(minor_list_choices) + list(option_list_choices) + list(complementary_modules),
-        ['title_fr', 'title_en'],
+        ['title_fr', 'title_en', 'changed'],
         batch_size=1000
     )
 
--- program_management/ddd/domain/service/generate_node_abbreviated_title.py
+++ program_management/ddd/domain/service/generate_node_abbreviated_title.py
@@ -24,7 +24,7 @@
 #
 ##############################################################################
 from base.models.education_group_year import EducationGroupYear
-from base.models.enums.education_group_types import EducationGroupTypesEnum
+from base.models.enums.education_group_types import EducationGroupTypesEnum, GroupType
 from osis_common.ddd import interface
 from program_management.ddd.business_types import *
 from program_management.ddd.domain.service.validation_rule import FieldValidationRule
@@ -35,9 +35,19 @@ class GenerateNodeAbbreviatedTitle(interface.DomainService):
     @classmethod
     def generate(cls, parent_node: 'Node', child_node_type: EducationGroupTypesEnum) -> str:
         default_abbreviated_title = FieldValidationRule.get(child_node_type, 'abbreviated_title').initial_value
-        default_title = FieldValidationRule.get(child_node_type, 'title_fr').initial_value
+        default_title = cls._get_default_title(child_node_type)
         default_value = default_abbreviated_title or default_title
         return "{child_title}{parent_abbreviated_title}".format(
             child_title=default_value.replace(" ", "").upper(),
             parent_abbreviated_title=parent_node.title
         )[:EducationGroupYear._meta.get_field("acronym").max_length]
+
+    @classmethod
+    def _get_default_title(cls, child_node_type):
+        default_specific_titles = {
+            GroupType.MINOR_LIST_CHOICE: 'Liste au choix mineures',
+            GroupType.OPTION_LIST_CHOICE: 'Liste au choix options'
+        }
+        if child_node_type in default_specific_titles.keys():
+            return default_specific_titles[child_node_type]
+        return FieldValidationRule.get(child_node_type, 'title_fr').initial_value
