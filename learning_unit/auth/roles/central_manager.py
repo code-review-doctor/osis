bebddc1ef708624af10ea279f93352c916c0944c
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2021-02-11 08:30:11 +0100

    OSIS-5554-bis : Modification de la permission pour la consolidation d'une proposition de suppression

--- learning_unit/auth/roles/central_manager.py
+++ learning_unit/auth/roles/central_manager.py
@@ -102,7 +102,7 @@ class CentralManager(osis_role_models.EntityRoleModel):
                  predicates.is_user_attached_to_requirement_entity) &
                 (predicates.is_not_proposal_of_type_suppression |
                     (predicates.has_learning_unit_no_application_all_years &
-                     predicates.has_learning_unit_no_attribution_all_years
+                     predicates.has_learning_unit_no_attribution_this_year
                      )
                  ),
             'base.can_add_charge_repartition':
