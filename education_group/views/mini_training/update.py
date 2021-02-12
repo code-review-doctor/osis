43bed9e254af8702fafa54cb947f9267ce0e0d31
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2021-02-12 10:39:44 +0100

    OSIS-4314 : Ajouter un message d'avertissement quand on modifie une of qui est utilisée dans plusieurs OF

--- education_group/views/mini_training/update.py
+++ education_group/views/mini_training/update.py
@@ -1,11 +1,11 @@
-# ############################################################################
+#############################################################################
 #  OSIS stands for Open Student Information System. It's an application
 #  designed to manage the core business of higher education institutions,
 #  such as universities, faculties, institutes and professional schools.
 #  The core business involves the administration of students, teachers,
 #  courses, programs and so on.
 #
-#  Copyright (C) 2015-2020 Université catholique de Louvain (http://www.uclouvain.be)
+#  Copyright (C) 2015-2021 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 #  A copy of this license - GNU General Public License - is available
 #  at the root of the source code of this program.  If not,
 #  see http://www.gnu.org/licenses/.
-# ############################################################################
+#############################################################################
 import functools
 from typing import List, Dict, Optional
 
@@ -35,7 +35,8 @@ from django.views import View
 from base.ddd.utils.business_validator import MultipleBusinessExceptions
 from base.utils import operator
 from base.utils.urls import reverse_with_get
-from base.views.common import display_success_messages, display_warning_messages, display_error_messages
+from base.views.common import display_success_messages, display_warning_messages, display_error_messages, \
+    check_formations_impacted_by_update
 from education_group.ddd import command
 from education_group.ddd.business_types import *
 from education_group.ddd.domain import exception
@@ -82,6 +83,9 @@ class MiniTrainingUpdateView(LoginRequiredMixin, PermissionRequiredMixin, View):
                 success_messages = self.get_success_msg_updated_mini_trainings(update_trainings)
                 success_messages += self.get_success_msg_deleted_mini_trainings(update_trainings)
                 display_success_messages(request, success_messages, extra_tags='safe')
+                check_formations_impacted_by_update(self.get_mini_training_obj().code,
+                                                    self.get_mini_training_obj().year,
+                                                    request)
                 return HttpResponseRedirect(self.get_success_url())
         display_error_messages(self.request, self._get_default_error_messages())
         return self.get(request, *args, **kwargs)
