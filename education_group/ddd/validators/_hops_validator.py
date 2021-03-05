14c588b2894802c15cd9cddca35d8b619f05a064
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2021-03-05 15:08:09 +0100

    OSIS-5636 : réparation bazar

--- education_group/ddd/validators/_hops_validator.py
+++ education_group/ddd/validators/_hops_validator.py
@@ -27,7 +27,7 @@ from base.ddd.utils.business_validator import MultipleBusinessExceptions
 from base.models.enums.education_group_types import TrainingType
 from education_group.ddd.domain.exception import HopsFieldsAllOrNone, \
     AresCodeShouldBeGreaterOrEqualsThanZeroAndLessThan9999, AresGracaShouldBeGreaterOrEqualsThanZeroAndLessThan9999, \
-    AresAuthorizationShouldBeGreaterOrEqualsThanZeroAndLessThan9999
+    AresAuthorizationShouldBeGreaterOrEqualsThanZeroAndLessThan9999, HopsFields2OrNoneForPhd
 
 
 class HopsValuesValidator(business_validator.BusinessValidator):
@@ -41,12 +41,17 @@ class HopsValuesValidator(business_validator.BusinessValidator):
             self.ares_authorization = training.hops.ares_authorization
         else:
             self.ares_code = self.ares_graca = self.ares_authorization = None
+        self.training_type = training.type
 
     def validate(self, *args, **kwargs):
         exceptions = []
         hops_fields_values = [value for value in [self.ares_code, self.ares_graca, self.ares_authorization] if value]
 
-        if 0 < len(hops_fields_values) < 3:
+        if self.training_type.name in (TrainingType.PHD.name, TrainingType.FORMATION_PHD.name):
+            if 0 < len(hops_fields_values) < 2 or \
+                    (len(hops_fields_values) == 2 and (self.ares_code is None or self.ares_authorization is None)):
+                exceptions.append(HopsFields2OrNoneForPhd())
+        elif 0 < len(hops_fields_values) < 3:
             exceptions.append(HopsFieldsAllOrNone())
         else:
             if self.ares_code and not 0 < self.ares_code <= 9999:
@@ -57,6 +62,5 @@ class HopsValuesValidator(business_validator.BusinessValidator):
 
             if self.ares_authorization and not 0 < self.ares_authorization <= 9999:
                 exceptions.append(AresAuthorizationShouldBeGreaterOrEqualsThanZeroAndLessThan9999())
-
         if exceptions:
             raise MultipleBusinessExceptions(exceptions=set(exceptions))
