84b62378810315bb2d3940b0470c5effa9492f7e
Eddy Ndizera <eddy.ndizera@uclouvain.be>
2021-03-05 07:10:44 +0100

    OSIS-5352 remove unused code

--- program_management/ddd/domain/program_tree_version.py
+++ program_management/ddd/domain/program_tree_version.py
@@ -261,17 +261,6 @@ class ProgramTreeVersion(interface.RootEntity):
     def is_standard_transition(self) -> bool:
         return self.entity_id.is_standard_transition
 
-    # FIXME :: à discuter de la manière de faire à cause de code presque dupliqué
-    @property
-    def version_label(self):
-        if self.entity_id.is_official_standard:
-            return ''
-        elif self.entity_id.is_standard_transition:
-            return '[{}]'.format(self.transition_name)
-        elif self.entity_id.is_specific_official:
-            return '[{}]'.format(self.version_name)
-        return '[{}-{}]'.format(self.version_name, self.transition_name)
-
     def update(self, data: UpdateProgramTreeVersiongData) -> 'ProgramTreeVersion':
         data_as_dict = attr.asdict(data, recurse=False)
         for field, new_value in data_as_dict.items():
