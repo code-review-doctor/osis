61a7f4085c38410db610fb486f50e583c8d531df
Laurent Spitaels <laurent.spitaels@uclouvain.be>
2021-03-10 11:47:23 +0100

    OSIS-5678 add predicates on partim

--- learning_unit/auth/roles/faculty_manager.py
+++ learning_unit/auth/roles/faculty_manager.py
@@ -36,6 +36,7 @@ class FacultyManager(osis_role_models.EntityRoleModel):
                 predicates.is_user_attached_to_current_requirement_entity &
                 predicates.is_learning_unit_start_year_after_year_limit &
                 predicates.has_learning_unit_prerequisite_dependencies &
+                predicates.has_learning_unit_partim_no_application_all_years &
                 predicates.has_learning_unit_no_application_all_years,
             'base.can_edit_learningunit':
                 predicates.is_user_attached_to_current_requirement_entity &
