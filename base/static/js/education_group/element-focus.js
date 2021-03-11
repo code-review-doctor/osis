806734b8d7fa9161a4d32c812f6565909ff9c35d
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2021-03-11 13:35:35 +0100

    OSIS-5673 : Suppression console.log

--- base/static/js/education_group/element-focus.js
+++ base/static/js/education_group/element-focus.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var eltidfocus = localStorage.getItem('eltidfocus');
-    console.log('avt if');
     if (eltidfocus){
-        console.log('if');
         var elt = document.getElementById(eltidfocus);
         if (elt){
-            console.log('avt if2');
             elt.focus();
             elt.scrollIntoView({'block': 'center', 'inline': 'center'});
         }
     }
 });
+
 $('.btn_to_focus').click(function (evt) {
-    console.log('sdjqsdf');
-    console.log(this.id);
     localStorage.setItem('eltidfocus', this.id);
 });
