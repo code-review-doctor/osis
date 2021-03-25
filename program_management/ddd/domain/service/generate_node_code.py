5cc8f35df7dcbbaab8f0a5740ac998e1a944d985
Eddy Ndizera <eddy.ndizera@uclouvain.be>
2021-03-25 07:43:47 +0100

    OSIS-1104 fix transition code generation

--- program_management/ddd/domain/service/generate_node_code.py
+++ program_management/ddd/domain/service/generate_node_code.py
@@ -23,6 +23,7 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+import copy
 import re
 from typing import Pattern, Tuple, List, Set, Iterable
 
@@ -45,33 +46,30 @@ REGEX_GROUP_PARTIAL_ACRONYM_INITIAL_VALUE = r"^(?P<cnum>\d{3})(?P<subdivision>[A
 MAX_CNUM = 999
 WIDTH_CNUM = 3
 
+REGEX_NUMERIC_PART = r"\d+"
+
 
 @attr.s()
 class GenerateNodeCode(interface.DomainService):
     existing_codes = attr.ib(type=Set[NodeCode], converter=set, default=attr.Factory(set))
 
     def generate_transition_code(self, base_code: NodeCode) -> NodeCode:
-        transition_base_code = "T" + base_code[1:]
-        new_code = self.__generate_node_code_from_code(transition_base_code)
+        transition_base_code = TRANSITION_FIRST_LETTER + base_code[1:]
 
+        new_code = self.__generate_node_code_from_code(transition_base_code)
         self.existing_codes.add(new_code)
+
         return new_code
 
     def __generate_node_code_from_code(self, base_code: str) -> str:
-        reg_parent_code = re.compile(REGEX_TRAINING_PARTIAL_ACRONYM)
-        reg_common_partial_acronym = re.compile(REGEX_COMMON_PARTIAL_ACRONYM)
-        reg_child_initial_value = re.compile(REGEX_GROUP_PARTIAL_ACRONYM_INITIAL_VALUE)
+        match_regex_numeric_part = re.search(REGEX_NUMERIC_PART, base_code)
+        start, end = match_regex_numeric_part.start(), match_regex_numeric_part.end()
+        numeric_part = int(base_code[start:end])
 
-        match_result = reg_parent_code.search(base_code) or reg_common_partial_acronym.search(base_code)
-        sigle_ele = match_result.group("sigle_ele")
-        cnum, subdivision = reg_child_initial_value.search(
-            base_code.replace(sigle_ele, "")
-        ).group("cnum", "subdivision")
-
-        code_generated = "{}{}{}".format(sigle_ele, cnum, subdivision)
+        code_generated = base_code
         while code_generated in self.existing_codes:
-            cnum = str(int(cnum) + 1)
-            code_generated = "{}{}{}".format(sigle_ele, cnum, subdivision)
+            numeric_part += 1
+            code_generated = "{}{}{}".format(code_generated[:start], str(numeric_part), code_generated[end:])
 
         return code_generated
 
