97b3e616f8a0aae93a78f09768907e02107ffeb5
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2021-03-30 15:46:13 +0200

    post workshop 5 : make ValidatorLists inherit from attr(frozen=True)

--- workshops_ddd_ue/validators/validators_by_business_action.py
+++ workshops_ddd_ue/validators/validators_by_business_action.py
@@ -2,23 +2,25 @@ from typing import List
 
 import attr
 
-from base.ddd.utils.business_validator import MultipleExceptionBusinessListValidator, \
-    TwoStepsMultipleBusinessExceptionListValidator, BusinessValidator
+from base.ddd.utils.business_validator import TwoStepsMultipleBusinessExceptionListValidator, BusinessValidator
+from workshops_ddd_ue.business_types import *
 from workshops_ddd_ue.command import CreateLearningUnitCommand
 from workshops_ddd_ue.domain.responsible_entity import ResponsibleEntity
 from workshops_ddd_ue.validators._should_academic_year_be_greater_than_2019 import ShouldAcademicYearGreaterThan2019
 from workshops_ddd_ue.validators._should_code_not_exist import ShouldCodeAlreadyExistsValidator
 from workshops_ddd_ue.validators._should_credits_respect_minimum_value import ShouldCreditsRespectMinimumValueValidator
-from workshops_ddd_ue.validators._should_internship_subtype_be_mandatory import ShouldInternshipSubtypeBeMandatoryValidator
-from workshops_ddd_ue.validators._should_learning_unit_code_respect_naming_convention import ShouldCodeRespectNamingConventionValidator
-from workshops_ddd_ue.validators._should_learning_unit_not_exists_in_next_year import ShouldLearningUnitNotExistNextYearValidator
 from workshops_ddd_ue.validators._should_fields_be_required import ShouldFieldsBeRequiredValidator
+from workshops_ddd_ue.validators._should_internship_subtype_be_mandatory import \
+    ShouldInternshipSubtypeBeMandatoryValidator
+from workshops_ddd_ue.validators._should_learning_unit_code_respect_naming_convention import \
+    ShouldCodeRespectNamingConventionValidator
+from workshops_ddd_ue.validators._should_learning_unit_not_exists_in_next_year import \
+    ShouldLearningUnitNotExistNextYearValidator
 from workshops_ddd_ue.validators._should_responsible_entity_have_authorized_type_or_code import \
     ShouldResponsibleEntityHaveAuthorizedTypeOrCode
 from workshops_ddd_ue.validators._subdivision_should_contain_only_one_letter import \
     SubdivisionShouldContainOnlyOneLetterValidator
 from workshops_ddd_ue.validators._subdivision_should_not_exist import SubdivisionShouldNotExistValidator
-from workshops_ddd_ue.business_types import *
 
 
 @attr.s(frozen=True, slots=True)
@@ -44,26 +46,35 @@ class CreateLearningUnitValidatorList(TwoStepsMultipleBusinessExceptionListValid
         ]
 
 
-class CopyLearningUnitToNextYearValidatorList(MultipleExceptionBusinessListValidator):
-    def __init__(
-            self,
-            learning_unit_identity: 'LearningUnitIdentity',
-            all_existing_lear_unit_identities: List['LearningUnitIdentity'],
-    ):
-        self.validators = [
-            ShouldLearningUnitNotExistNextYearValidator(learning_unit_identity, all_existing_lear_unit_identities),
+@attr.s(frozen=True, slots=True)
+class CopyLearningUnitToNextYearValidatorList(TwoStepsMultipleBusinessExceptionListValidator):
+
+    learning_unit_identity = attr.ib(type=LearningUnitIdentity)
+    all_existing_lear_unit_identities = attr.ib(type=List['LearningUnitIdentity'])  # type: List['LearningUnitIdentity']
+
+    def get_data_contract_validators(self) -> List[BusinessValidator]:
+        return []
+
+    def get_invariants_validators(self) -> List[BusinessValidator]:
+        return [
+            ShouldLearningUnitNotExistNextYearValidator(
+                self.learning_unit_identity,
+                self.all_existing_lear_unit_identities
+            ),
         ]
-        super().__init__()
 
 
-class CreatePartimValidatorList(MultipleExceptionBusinessListValidator):
-    def __init__(
-            self,
-            learning_unit: 'LearningUnit',
-            subdivision: str,
-    ):
-        self.validators = [
-            SubdivisionShouldContainOnlyOneLetterValidator(subdivision),
-            SubdivisionShouldNotExistValidator(subdivision, learning_unit),
+@attr.s(frozen=True, slots=True)
+class CreatePartimValidatorList(TwoStepsMultipleBusinessExceptionListValidator):
+
+    learning_unit = attr.ib(type=LearningUnit)
+    subdivision = attr.ib(type=str)
+
+    def get_data_contract_validators(self) -> List[BusinessValidator]:
+        return []
+
+    def get_invariants_validators(self) -> List[BusinessValidator]:
+        return [
+            SubdivisionShouldContainOnlyOneLetterValidator(self.subdivision),
+            SubdivisionShouldNotExistValidator(self.subdivision, self.learning_unit),
         ]
-        super().__init__()
