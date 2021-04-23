b08d7a60291d7b2d1233a64011927ebe5e71c9d4
Laurent Spitaels <laurent.spitaels@uclouvain.be>
2021-04-23 08:32:32 +0200

    OSIS-5688 adapt if not instance of academic_year

--- education_group/forms/training.py
+++ education_group/forms/training.py
@@ -356,10 +356,14 @@ class CreateTrainingForm(ValidationRuleMixin, forms.Form):
         )
 
     def __init_management_entity_field(self):
+        academic_year = self.initial.get('academic_year')
+        if not isinstance(academic_year, AcademicYear):
+            academic_year = AcademicYear.objects.get(year=self.initial.get('academic_year'))
         self.fields['management_entity'] = fields.ManagementEntitiesModelChoiceField(
             person=self.user.person,
             initial=self.initial.get('management_entity'),
             disabled=self.fields['management_entity'].disabled,
+            academic_year=academic_year
         )
 
     def __init_certificate_aims_field(self):
