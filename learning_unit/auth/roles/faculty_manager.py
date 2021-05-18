1ffe821b14da66d409987ba9196272b10048b54c
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2021-05-18 16:01:39 +0200

    OSIS-5821 : adapt predicates for attribution

--- learning_unit/auth/roles/faculty_manager.py
+++ learning_unit/auth/roles/faculty_manager.py
@@ -114,7 +114,7 @@ class FacultyManager(osis_role_models.EntityRoleModel):
                  predicates.is_user_attached_to_requirement_entity) &
                 (predicates.is_not_proposal_of_type_suppression |
                  (predicates.has_learning_unit_no_application_now_and_in_future &
-                  predicates.has_learning_unit_no_attribution_this_year
+                  predicates.has_learning_unit_no_attribution_now_and_future
                   )
                  ),
             'base.can_add_charge_repartition':
