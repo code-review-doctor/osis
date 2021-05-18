980a79375c9098306a08aef806a2bbb15b64da6b
Eddy Ndizera <eddy.ndizera@uclouvain.be>
2021-05-18 13:13:42 +0200

    Fix random failling tests

--- cms/tests/models/test_text_label.py
+++ cms/tests/models/test_text_label.py
@@ -24,6 +24,7 @@
 #
 ##############################################################################
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from django.test import TestCase
 
 from cms.models import text_label
@@ -179,11 +180,6 @@ class TextLabelComplexeStructureTest(TestCase):
         self.assertEqual(self.C.order, 1)  # Inversion
 
     def test_change_on_same_level_no_change_as_result(self):
-        self.E.order = 2
-        self.E.save()
-        self.F.refresh_from_db()
-        self.G.refresh_from_db()
-        # No change because we have reorder value
-        self.assertEqual(self.E.order, 1)
-        self.assertEqual(self.F.order, 2)
-        self.assertEqual(self.G.order, 3)
+        with self.assertRaises(IntegrityError):
+            self.E.order = 2
+            self.E.save()
