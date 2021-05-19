fada70f056b4b6a88cced4b7e87b2ab6e38a385b
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2021-05-19 15:55:39 +0200

    Correction script

--- manage_submodules.py
+++ manage_submodules.py
@@ -2,12 +2,20 @@
 
 import os.path
 from os import path
+import subprocess
+import sys
 
-branch = "dev"
 project_dir = os.getcwd()
-
+project_branch = subprocess.check_output('git branch --show-current', shell=True).decode(sys.stdout.encoding)
+module_branch = 'dev'
+arguments = len(sys.argv) - 1
+if arguments > 0:
+    module_branch = sys.argv[1]
+elif project_branch in ['dev', 'test', 'qa', 'master']:
+    module_branch = project_branch
 print('Project Dir : {}'.format(project_dir))
-print('Branch : {}'.format(branch))
+print('Project Branch : {}'.format(project_branch))
+print('Module Branch : {}'.format(module_branch))
 print('')
 
 modules = {
@@ -27,18 +35,26 @@ for module, git_url in modules.items():
     if path.exists(module_dir) and path.isdir(module_dir):
         print('')
         print('Module {} exists in project directory'.format(module))
-        print('Updating module {} from branch {}'.format(module, branch))
-        checkout_command = 'git checkout {branch}'.format(branch=branch)
+        print('Updating module {} from branch {}'.format(module, module_branch))
+        fetch_command = 'git fetch origin {branch}'.format(branch=module_branch)
+        switch_branch_command = 'git checkout {branch}'.format(branch=module_branch)
+        new_branch_command = 'git checkout -b {branch} origin/{branch}'.format(branch=module_branch)
+        check_branch_exists_locally_command = ' git rev-parse --verify {branch}'.format(branch=module_branch)
         pull_command = 'git pull'
         os.chdir(module_dir)
-        os.system(checkout_command)
+        os.system(fetch_command)
+        try:
+            command_status = subprocess.check_call(check_branch_exists_locally_command.split())
+            os.system(switch_branch_command)
+        except subprocess.CalledProcessError:
+            os.system(new_branch_command)
         os.system(pull_command)
         os.chdir(project_dir)
     else:
         print('')
         print('Module {} not exists in project directory'.format(module))
-        print('Cloning module {} from branch {} in directory {}'.format(module, branch, module_dir))
-        command = 'git clone {git_url} -b {branch} --single-branch {directory}'.format(branch=branch,
-                                                                                       git_url=git_url,
-                                                                                       directory=module_dir)
+        print('Cloning module {} from branch {} in directory {}'.format(module, module_branch, module_dir))
+        command = 'git clone {git_url} -b {branch} {directory}'.format(branch=module_branch,
+                                                                       git_url=git_url,
+                                                                       directory=module_dir)
         os.system(command)
