685c75a6057832ac10f6a543cd4e533491db1629
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2021-06-07 08:42:24 +0200

    OSIS-4270 : remove useless factory

--- ddd/logic/learning_unit/test/use_case/read/test_get_learning_unit_service.py
+++ ddd/logic/learning_unit/test/use_case/read/test_get_learning_unit_service.py
@@ -28,7 +28,6 @@ from django.test import SimpleTestCase
 
 from ddd.logic.learning_unit.commands import GetLearningUnitCommand
 from ddd.logic.learning_unit.test.factory.learning_unit import LDROI1001CourseLearningUnitFactory
-from ddd.logic.learning_unit.test.factory.ucl_entity import DRTEntityFactory
 from ddd.logic.learning_unit.use_case.read import get_learning_unit_service
 from infrastructure.learning_unit.repository.in_memory.learning_unit import LearningUnitRepository
 
@@ -37,7 +36,6 @@ class TestGetLearningUnitService(SimpleTestCase):
 
     def setUp(self):
         self.learning_unit_repository = LearningUnitRepository()
-        self.fac_drt = DRTEntityFactory()
         self.learning_unit = LDROI1001CourseLearningUnitFactory()
         self.learning_unit_repository.save(self.learning_unit)
         self.command = GetLearningUnitCommand(code=self.learning_unit.code, year=self.learning_unit.year)
