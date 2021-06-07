3b7db3ba335664a5c802dd60769fefc11c78cb8d
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2021-06-07 08:36:26 +0200

    OSIS-4270 : use factory

--- ddd/logic/learning_unit/test/use_case/read/test_get_learning_unit_service.py
+++ ddd/logic/learning_unit/test/use_case/read/test_get_learning_unit_service.py
@@ -26,11 +26,8 @@
 
 from django.test import SimpleTestCase
 
-from base.models.enums.learning_container_year_types import LearningContainerYearType
-from base.models.enums.learning_unit_year_periodicity import PeriodicityEnum
-from base.models.enums.quadrimesters import DerogationQuadrimester
-from ddd.logic.learning_unit.builder.learning_unit_builder import LearningUnitBuilder
-from ddd.logic.learning_unit.commands import CreateLearningUnitCommand, GetLearningUnitCommand
+from ddd.logic.learning_unit.commands import GetLearningUnitCommand
+from ddd.logic.learning_unit.test.factory.learning_unit import LDROI1001CourseLearningUnitFactory
 from ddd.logic.learning_unit.test.factory.ucl_entity import DRTEntityFactory
 from ddd.logic.learning_unit.use_case.read import get_learning_unit_service
 from infrastructure.learning_unit.repository.in_memory.learning_unit import LearningUnitRepository
@@ -41,12 +38,7 @@ class TestGetLearningUnitService(SimpleTestCase):
     def setUp(self):
         self.learning_unit_repository = LearningUnitRepository()
         self.fac_drt = DRTEntityFactory()
-        lu_command = _get_create_learning_unit_command()
-        self.learning_unit = LearningUnitBuilder.build_from_command(
-            cmd=lu_command,
-            all_existing_identities=[],
-            responsible_entity_identity=self.fac_drt.entity_id
-        )
+        self.learning_unit = LDROI1001CourseLearningUnitFactory()
         self.learning_unit_repository.save(self.learning_unit)
         self.command = GetLearningUnitCommand(code=self.learning_unit.code, year=self.learning_unit.year)
 
@@ -59,32 +51,3 @@ class TestGetLearningUnitService(SimpleTestCase):
         fields = vars(self.learning_unit)
         for field in fields:
             self.assertEqual(getattr(learning_unit, field), getattr(self.learning_unit, field), field)
-
-
-def _get_create_learning_unit_command():
-    lu_command = CreateLearningUnitCommand(
-        code="LDROI1001",
-        academic_year=2020,
-        type=LearningContainerYearType.COURSE.name,
-        common_title_fr="Introduction au droit",
-        specific_title_fr="Partie 1 : droit civil",
-        common_title_en="Introduction to law",
-        specific_title_en="Part 1 : civic law",
-        credits=7.0,
-        internship_subtype=None,
-        responsible_entity_code=self.fac_drt.entity_id.code,
-        periodicity=PeriodicityEnum.ANNUAL.name,
-        iso_code="fr-be",
-        remark_faculty="Remark fac",
-        remark_publication_fr="Remarqué publiée sur le portail",
-        remark_publication_en="Remark published",
-        practical_volume_q1=5.0,
-        practical_volume_q2=15.0,
-        practical_volume_annual=20.0,
-        lecturing_volume_q1=25.0,
-        lecturing_volume_q2=35.0,
-        lecturing_volume_annual=60.0,
-        derogation_quadrimester=DerogationQuadrimester.Q1and2.name,
-        teaching_place_uuid=None
-    )
-    return lu_command
