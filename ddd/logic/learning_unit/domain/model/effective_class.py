fb7bf65450fe616ab6e76f3b36995d38757d7034
mathieuzen <zen.mathieu@gmail.com>
2021-06-17 12:35:11 +0200

    OSIS-5900 simplify tutor tab using is_lecturing or is_practical prop

--- ddd/logic/learning_unit/domain/model/effective_class.py
+++ ddd/logic/learning_unit/domain/model/effective_class.py
@@ -74,8 +74,14 @@ class EffectiveClass(interface.RootEntity, abc.ABC):
 
 
 class PracticalEffectiveClass(EffectiveClass):
-    pass
+
+    @property
+    def is_practical(self):
+        return True
 
 
 class LecturingEffectiveClass(EffectiveClass):
-    pass
+
+    @property
+    def is_lecturing(self):
+        return True
