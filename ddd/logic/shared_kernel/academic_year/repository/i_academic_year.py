ff33af34145c08a18e95b6ee5fb42cb1c0ce5c51
Christophe Fivet <christophe.fivet@uclouvain.be>
2021-06-18 14:31:44 +0200

    CHange variable name

--- ddd/logic/shared_kernel/academic_year/repository/i_academic_year.py
+++ ddd/logic/shared_kernel/academic_year/repository/i_academic_year.py
@@ -39,7 +39,7 @@ class IAcademicYearRepository(interface.AbstractRepository):
 
     @classmethod
     @abstractmethod
-    def search(cls, early_limit_year: Optional[int], **kwargs) -> List['AcademicYear']:
+    def search(cls, year: Optional[int], **kwargs) -> List['AcademicYear']:
         pass
 
     @classmethod
