2e11bfb6e9704a5a69df7a2b1022ac2f090326ad
Eddy Ndizera <eddy.ndizera@uclouvain.be>
2021-06-25 10:52:33 +0200

    OSIS-4125 remove schedule task

--- learning_unit/tasks/__init__.py
+++ learning_unit/tasks/__init__.py
@@ -8,11 +8,3 @@ celery_app.conf.beat_schedule.update({
         'schedule': crontab(minute=0, hour=0, day_of_month='*', month_of_year='*', day_of_week=0)
     },
 })
-
-
-celery_app.conf.beat_schedule.update({
-    '|Learning Unit| General postponement of learning unit until n+6': {
-        'task': 'learning_unit.tasks.postpone_learning_units_until_n_plus_6.run',
-        'schedule': crontab(minute=0, hour=0, day_of_month='2', month_of_year='7', day_of_week=0)
-    },
-})
