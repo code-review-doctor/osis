488ba72449dbcb1b5e5a27e609b9af488accbb3a
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2021-07-01 08:29:21 +0200

    OSIS-5931 : Ajustement pour que les traductions des types de stage soient bien traduit dans les messages d'avertissement

--- base/forms/learning_unit/learning_unit_postponement.py
+++ base/forms/learning_unit/learning_unit_postponement.py
@@ -39,6 +39,7 @@ from base.models import academic_year
 from base.models.academic_year import AcademicYear
 from base.models.enums import learning_unit_year_subtypes
 from base.models.enums import quadrimesters
+from base.models.enums.internship_subtypes import INTERNSHIP_SUBTYPES
 from base.models.enums.learning_component_year_type import LECTURING
 from base.models.enums.learning_unit_year_periodicity import PERIODICITY_TYPES
 from base.models.learning_component_year import LearningComponentYear
@@ -389,6 +390,8 @@ class LearningUnitPostponementForm:
             return _("yes") if value else _("no")
 
         if value:
+            if field_name == 'internship_subtype':
+                return dict(INTERNSHIP_SUBTYPES)[value].lower()
             if field_name == 'periodicity':
                 return dict(PERIODICITY_TYPES)[value].lower()
             elif field_name == 'quadrimester':
