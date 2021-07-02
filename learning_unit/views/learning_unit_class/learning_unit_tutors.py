7677158aa55e18b0e3c7bff9ce7f0233d25680b8
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2021-07-02 15:30:50 +0200

    OSIS-4287 : Refactor

--- learning_unit/views/learning_unit_class/learning_unit_tutors.py
+++ learning_unit/views/learning_unit_class/learning_unit_tutors.py
@@ -25,63 +25,33 @@
 ##############################################################################
 from typing import List
 
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.utils.functional import cached_property
 from django.views.generic import TemplateView
 
 from base.models.enums.component_type import LECTURING, PRACTICAL_EXERCISES
 from ddd.logic.attribution.commands import SearchAttributionsToLearningUnitCommand
 from ddd.logic.attribution.dtos import TutorAttributionToLearningUnitDTO
-from ddd.logic.learning_unit.commands import GetEffectiveClassCommand
 from ddd.logic.learning_unit.domain.model.effective_class import EffectiveClass, PracticalEffectiveClass
 from infrastructure.messages_bus import message_bus_instance
-from learning_unit.models.learning_class_year import LearningClassYear
-from learning_unit.views.learning_unit_class.common import common_url_tabs
+from learning_unit.views.learning_unit_class.common import CommonClassView
 
 
-class LearningUnitTutorsView(PermissionRequiredMixin, TemplateView):
+class LearningUnitTutorsView(CommonClassView, TemplateView):
     template_name = "class/lu_tutors.html"
     permission_required = 'base.can_access_class'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        effective_class = self.effective_class
         context.update(
             {
-                'effective_class': effective_class,
+                'effective_class': self.effective_class,
                 'tutors':
                     self.get_ue_tutors(),
-                'can_add_charge_repartition': True,   # TODO je ne connais pas la condition,
+                'can_add_charge_repartition': True,  # TODO je ne connais pas la condition,
                 'can_delete_attribution': True  # TODO je ne connais pas la condition,
             }
         )
-        context.update(
-            common_url_tabs(
-                effective_class.entity_id.learning_unit_identity.code,
-                effective_class.entity_id.learning_unit_identity.year,
-                effective_class.entity_id.class_code
-            )
-        )
+        context.update(self.common_url_tabs())
         return context
-
-    def get_permission_object(self):
-        return LearningClassYear.objects.filter(
-            acronym=self.kwargs['class_code'],
-            learning_component_year__learning_unit_year__academic_year__year=self.kwargs['learning_unit_year'],
-            learning_component_year__learning_unit_year__acronym=self.kwargs['learning_unit_code']
-        ).select_related(
-            'learning_component_year__learning_unit_year',
-            'learning_component_year__learning_unit_year__academic_year'
-        )
-
-    @cached_property
-    def effective_class(self) -> 'EffectiveClass':
-        command = GetEffectiveClassCommand(
-            class_code=self.kwargs['class_code'],
-            learning_unit_code=self.kwargs['learning_unit_code'],
-            learning_unit_year=self.kwargs['learning_unit_year']
-        )
-        return message_bus_instance.invoke(command)
 
     def get_ue_tutors(self) -> List['TutorAttributionToLearningUnitDTO']:
         tutors = message_bus_instance.invoke(
