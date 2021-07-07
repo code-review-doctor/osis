b1c4efbea7f223f0d4b298a39b6858d13f33d8b5
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2021-07-07 12:46:19 +0200

    partial commit :: add subcontext 'encodage' (en tant que gestionnaire)

---
+++ ddd/logic/encodage_des_notes/encodage/domain/model/_note.py
@@ -0,0 +1,22 @@
+import abc
+from typing import Any
+
+import attr
+
+from osis_common.ddd import interface
+
+
+class Note(interface.ValueObject, abc.ABC):
+    value = attr.ib(type=Any)
+
+
+class NoteChiffree(Note):
+    value = attr.ib(type=int)
+
+
+class NoteManquante(Note):
+    value = ""
+
+
+class Justification(Note):
+    value = attr.ib(type=str)  # TODO : remplacer avec Enum (avec "absence justifiée")
