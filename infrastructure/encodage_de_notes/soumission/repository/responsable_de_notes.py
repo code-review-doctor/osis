babf92054ef0954af868923d4c7101053bfa76ef
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2021-07-07 12:52:32 +0200

    add new repository interface 'IResponsableDeNotesRepository'

---
+++ infrastructure/encodage_de_notes/soumission/repository/responsable_de_notes.py
@@ -0,0 +1,57 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2021 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from typing import Optional, List
+
+from ddd.logic.encodage_des_notes.soumission.domain.model.responsable_de_notes import IdentiteResponsableDeNotes, \
+    ResponsableDeNotes
+from ddd.logic.encodage_des_notes.soumission.repository.i_responsable_de_notes import IResponsableDeNotesRepository
+from osis_common.ddd.interface import ApplicationService
+
+
+class ResponsableDeNotesRepositoryRepository(IResponsableDeNotesRepository):
+    @classmethod
+    def search(
+            cls,
+            entity_ids: Optional[List['IdentiteResponsableDeNotes']] = None,
+            **kwargs
+    ) -> List['ResponsableDeNotes']:
+        raise NotImplementedError
+
+    @classmethod
+    def delete(cls, entity_id: 'IdentiteResponsableDeNotes', **kwargs: ApplicationService) -> None:
+        raise NotImplementedError
+
+    @classmethod
+    def save(cls, entity: 'ResponsableDeNotes') -> None:
+        raise NotImplementedError
+
+    @classmethod
+    def get_all_identities(cls) -> List['IdentiteResponsableDeNotes']:
+        raise NotImplementedError
+
+    @classmethod
+    def get(cls, entity_id: 'IdentiteResponsableDeNotes') -> 'ResponsableDeNotes':
+        raise NotImplementedError
