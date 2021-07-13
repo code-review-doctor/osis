d31f0a1c6b18f5a1671ce60000ba6040586bd0d6
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2021-07-13 10:16:09 +0200

    OSIS-5929

--- base/business/learning_unit_xls.py
+++ base/business/learning_unit_xls.py
@@ -318,7 +318,7 @@ def get_data_part2(learning_unit_yr: LearningUnitYear, with_attributions: bool) 
         learning_unit_yr.get_quadrimester_display() or '',
         learning_unit_yr.get_session_display() or '',
         learning_unit_yr.language or "",
-        yesno(learning_unit_yr.stage_dimona).strip()
+        yesno(learning_unit_yr.stage_dimona).strip(),
     ])
     return lu_data_part2
 
