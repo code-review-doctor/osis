cc08601b1b2d7fde94e9fac954561fc49e648155
Eddy Ndizera <eddy.ndizera@uclouvain.be>
2021-07-26 11:58:44 +0200

    execute ddd + infrastructe tests + fix tests

--- backoffice/settings/base.py
+++ backoffice/settings/base.py
@@ -144,6 +144,8 @@ APPS_TO_TEST = (
     'education_group',
     'learning_unit',
     'program_management',
+    'ddd',
+    'infrastructure',
 )
 TEST_RUNNER = os.environ.get('TEST_RUNNER', 'osis_common.tests.runner.InstalledAppsTestRunner')
 SKIP_QUEUES_TESTS = os.environ.get('SKIP_QUEUES_TESTS', 'False').lower() == 'true'
