2a98e9b0f19d37090ddd9af0e07fddbe844411d5
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2021-07-26 17:37:53 +0200

    Ajout arguments module+module_branch

--- manage_submodules.py
+++ manage_submodules.py
@@ -7,17 +7,8 @@ import sys
 
 project_dir = os.getcwd()
 project_branch = subprocess.check_output('git branch --show-current', shell=True).decode(sys.stdout.encoding)
-module_branch = 'dev'
-arguments = len(sys.argv) - 1
-if arguments > 0:
-    module_branch = sys.argv[1]
-elif project_branch in ['dev', 'test', 'qa', 'master']:
-    module_branch = project_branch
-print('Project Dir : {}'.format(project_dir))
-print('Project Branch : {}'.format(project_branch))
-print('Module Branch : {}'.format(module_branch))
-print('')
-
+default_module_branch = 'dev'
+modules_branches = {}
 modules = {
     "admission": "git@github.com:uclouvain/osis-admission.git",
     "assistant": "git@github.com:uclouvain/osis-assistant.git",
@@ -29,18 +20,42 @@ modules = {
     "osis_mail_template": "git@github.com:uclouvain/osis-mail-template.git",
     "partnership": "git@github.com:uclouvain/osis-partnership.git",
 }
+arguments = len(sys.argv) - 1
+if arguments > 0:
+    default_module_branch = sys.argv[1]
+    if arguments > 1:
+        if arguments % 2 == 0:
+            raise Exception("Le nombre d'arguments doit être impair : "
+                            "la branche par défaut en premier et un suite de couple nom_dule/branhc_module")
+        idx = 2
+        while idx < arguments:
+            module_name = sys.argv[idx]
+            if module_name not in modules:
+                raise Exception("Le module {} n'est pas dans la liste des modules".format(module_name))
+            idx = idx + 1
+            module_branch = sys.argv[idx]
+            modules_branches[module_name] = module_branch
 
-# TODO :: ajouter un 2e param "nom_du_submodule" pour faciliter le déploiement par branche pour chaque submodule + adapter config travis
+elif project_branch in ['dev', 'test', 'qa', 'master']:
+    default_module_branch = project_branch
+print('Project Dir : {}'.format(project_dir))
+print('Project Branch : {}'.format(project_branch))
+print('Module Branch : {}'.format(default_module_branch))
+print('')
+
 for module, git_url in modules.items():
     module_dir = os.path.join(project_dir, module)
     if path.exists(module_dir) and path.isdir(module_dir):
         print('')
         print('Module {} exists in project directory'.format(module))
-        print('Updating module {} from branch {}'.format(module, module_branch))
-        fetch_command = 'git fetch origin {branch}'.format(branch=module_branch)
-        switch_branch_command = 'git checkout {branch}'.format(branch=module_branch)
-        new_branch_command = 'git checkout -b {branch} origin/{branch}'.format(branch=module_branch)
-        check_branch_exists_locally_command = ' git rev-parse --verify {branch}'.format(branch=module_branch)
+        print('Updating module {} from branch {}'.format(module, default_module_branch))
+        specific_module_branch = default_module_branch
+        if module in modules_branches:
+            specific_module_branch = modules_branches.get(module)
+        fetch_command = 'git fetch origin {branch}'.format(branch=specific_module_branch)
+        switch_branch_command = 'git checkout {branch}'.format(branch=specific_module_branch)
+        new_branch_command = 'git checkout -b {branch} origin/{branch}'.format(branch=specific_module_branch)
+        check_branch_exists_locally_command = ' git rev-parse --verify {branch}'.format(branch=specific_module_branch)
         pull_command = 'git pull'
         os.chdir(module_dir)
         os.system(fetch_command)
@@ -54,8 +69,8 @@ for module, git_url in modules.items():
     else:
         print('')
         print('Module {} not exists in project directory'.format(module))
-        print('Cloning module {} from branch {} in directory {}'.format(module, module_branch, module_dir))
-        command = 'git clone {git_url} -b {branch} {directory}'.format(branch=module_branch,
+        print('Cloning module {} from branch {} in directory {}'.format(module, specific_module_branch, module_dir))
+        command = 'git clone {git_url} -b {branch} {directory}'.format(branch=specific_module_branch,
                                                                        git_url=git_url,
                                                                        directory=module_dir)
         os.system(command)
