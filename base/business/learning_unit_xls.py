f20328160f8a889732c7355cfc0a75e249442437
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2021-08-17 14:47:51 +0200

    OSIS-5978

--- base/business/learning_unit_xls.py
+++ base/business/learning_unit_xls.py
@@ -516,6 +516,7 @@ def prepare_xls_content_with_attributions(found_learning_units: QuerySet, nb_col
     cells_with_top_border = []
     cells_with_white_font = []
     line = 2
+    nb_columns_which_could_be_whitened = 29
 
     for learning_unit_yr in qs:
         first = True
@@ -542,7 +543,10 @@ def prepare_xls_content_with_attributions(found_learning_units: QuerySet, nb_col
                 line += 1
                 if not first:
                     cells_with_white_font.extend(
-                        ["{}{}".format(letter, line-1) for letter in _get_all_columns_reference(29)]
+                        [
+                            "{}{}".format(letter, line-1)
+                            for letter in _get_all_columns_reference(nb_columns_which_could_be_whitened)
+                        ]
                     )
                 first = False
         else:
@@ -569,7 +573,10 @@ def prepare_xls_content_with_attributions(found_learning_units: QuerySet, nb_col
                     line += 1
                     if not first_attribution:
                         cells_with_white_font.extend(
-                            ["{}{}".format(letter, line - 1) for letter in _get_all_columns_reference(24, 1)]
+                            [
+                                "{}{}".format(letter, line - 1)
+                                for letter in _get_all_columns_reference(nb_columns_which_could_be_whitened, 1)
+                            ]
                         )
                     first_attribution = False
             else:
