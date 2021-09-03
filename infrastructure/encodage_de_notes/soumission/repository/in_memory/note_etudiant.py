2dbd30839e3335b6e2d0634b17740491dff81247
unknown <alessandro.brugnetti@uclouvain.be>
2021-09-03 16:04:21 +0200

    Implement search dates echeances

--- infrastructure/encodage_de_notes/soumission/repository/in_memory/note_etudiant.py
+++ infrastructure/encodage_de_notes/soumission/repository/in_memory/note_etudiant.py
@@ -23,10 +23,11 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from typing import List
+from typing import List, Set
 
 from base.ddd.utils.in_memory_repository import InMemoryGenericRepository
-from ddd.logic.encodage_des_notes.soumission.domain.model.note_etudiant import NoteEtudiant
+from ddd.logic.encodage_des_notes.soumission.domain.model.note_etudiant import NoteEtudiant, IdentiteNoteEtudiant
+from ddd.logic.encodage_des_notes.soumission.dtos import DateEcheanceNoteDTO
 from ddd.logic.encodage_des_notes.soumission.repository.i_note_etudiant import INoteEtudiantRepository, SearchCriteria
 
 
@@ -42,4 +43,27 @@ class NoteEtudiantInMemoryRepository(InMemoryGenericRepository, INoteEtudiantRep
             entity
             for entity in cls.entities
             if (entity.code_unite_enseignement, entity.annee, entity.numero_session) in criterias
+        ]
+
+    @classmethod
+    def search_dates_echeances(
+            cls,
+            notes_identites: Set[IdentiteNoteEtudiant]
+    ) -> List[DateEcheanceNoteDTO]:
+        entities_sorted = sorted(cls.entities, key=lambda entity: (
+            entity.code_unite_enseignement,
+            entity.annee,
+            entity.date_limite_de_remise.to_date(),
+        ))
+        return [
+            DateEcheanceNoteDTO(
+                code_unite_enseignement=entity.code_unite_enseignement,
+                annee_unite_enseignement=entity.annee,
+                numero_session=entity.numero_session,
+                noma=entity.noma,
+                jour=entity.date_limite_de_remise.jour,
+                mois=entity.date_limite_de_remise.mois,
+                annee=entity.date_limite_de_remise.annee,
+                note_soumise=entity.est_soumise,
+            ) for entity in entities_sorted
         ]
