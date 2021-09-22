91e692c476aedf8308e6f341025298452c05b48b
Christophe Fivet <christophe.fivet@uclouvain.be>
2021-09-22 14:55:45 +0200

    OSIS-6130 FIxed Issue 1

--- ddd/logic/shared_kernel/entite/domain/model/entiteucl.py
+++ ddd/logic/shared_kernel/entite/domain/model/entiteucl.py
@@ -54,3 +54,6 @@ class EntiteUCL(interface.RootEntity):
 
     def est_faculte(self) -> bool:
         return self.type == EntityType.FACULTY
+
+    def est_sector(self) -> bool:
+        return self.type == EntityType.SECTOR
