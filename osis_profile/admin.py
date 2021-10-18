75906416ca617f9da40cdbc0ca127cf41e146267
jmr <jmr@makina-corpus.com>
2021-10-18 10:53:26 +0200

    high school models basic admin

--- osis_profile/admin.py
+++ osis_profile/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from osis_profile.models import BelgianHighSchoolDiploma, ForeignHighSchoolDiploma
+from osis_profile.models.education import Schedule
+
+
+class HighSchoolDiplomaAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["person"]
+
+
+admin.site.register(BelgianHighSchoolDiploma, HighSchoolDiplomaAdmin)
+admin.site.register(ForeignHighSchoolDiploma, HighSchoolDiplomaAdmin)
+admin.site.register(Schedule)
