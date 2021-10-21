ea7635c909029ec62cdca7ff524b9f311b99d69d
Laurent Spitaels <laurent.spitaels@uclouvain.be>
2021-10-21 13:52:30 +0200

    OSIS-4305 init commit

--- assessments/views/program_manager/scores_responsible.py
+++ assessments/views/program_manager/scores_responsible.py
@@ -36,6 +36,7 @@ from django_filters.views import FilterView
 from assessments.api.serializers.scores_responsible import ScoresResponsibleListSerializer
 from assessments.forms.scores_responsible import ScoresResponsiblesFilter
 from assessments.models.score_responsible import ScoreResponsible
+from attribution.models.attribution_class import AttributionClass
 from attribution.models.attribution_new import AttributionNew
 from base.models import session_exam_calendar
 from base.models.academic_year import AcademicYear
@@ -43,10 +44,12 @@ from base.models.learning_unit_year import LearningUnitYear
 from base.utils.cache import CacheFilterMixin
 from ddd.logic.encodage_des_notes.soumission.commands import AssignerResponsableDeNotesCommand
 from infrastructure.messages_bus import message_bus_instance
+from base.models.enums.learning_component_year_type import LECTURING
 
 
 @attr.s(frozen=True, slots=True)
 class AttributionDTO:
+    code = attr.ib(type=str)
     matricule_fgs = attr.ib(type=str)
     enseignant = attr.ib(type=str)
     statut = attr.ib(type=str)
@@ -109,6 +112,7 @@ class SelectScoreResponsible(LoginRequiredMixin, PermissionRequiredMixin, Templa
 
         return [
             AttributionDTO(
+                code=self.code,
                 matricule_fgs=attribution_new.tutor.person.global_id,
                 enseignant=str(attribution_new.tutor),
                 statut=attribution_new.get_function_display(),
@@ -116,6 +120,49 @@ class SelectScoreResponsible(LoginRequiredMixin, PermissionRequiredMixin, Templa
             )
             for attribution_new in qs
         ]
+
+    def get_attributions_classes(self):
+        qs = AttributionClass.objects.filter(
+            attribution_charge__learning_component_year__learning_unit_year__acronym=self.code,
+            attribution_charge__learning_component_year__learning_unit_year__academic_year=self.academic_year,
+        ).annotate(
+            est_responsable_de_notes=Exists(
+                ScoreResponsible.objects.filter(
+                    learning_unit_year__acronym=self.code,
+                    learning_unit_year__academic_year=self.academic_year,
+                    learning_class_year__isnull=False,
+                    tutor=OuterRef('attribution_charge__attribution__tutor')
+                )
+            )
+        ).order_by(
+            "learning_class_year__acronym",
+            "attribution_charge__attribution__tutor__person__last_name",
+            "attribution_charge__attribution__tutor__person__first_name",
+        ).distinct(
+            "learning_class_year__acronym",
+            "attribution_charge__attribution__tutor__person__last_name",
+            "attribution_charge__attribution__tutor__person__first_name",
+        )
+
+        attribution_classes_dtos = dict()
+        for attribution_class in qs:
+            if attribution_class.learning_class_year.learning_component_year.type == LECTURING:
+                code_classe = self.code + '-' + attribution_class.learning_class_year.acronym
+            else:
+                code_classe = self.code + '_' + attribution_class.learning_class_year.acronym
+            attribution_dto = AttributionDTO(
+                code=code_classe,
+                matricule_fgs=attribution_class.attribution_charge.attribution.tutor.person.global_id,
+                enseignant=str(attribution_class.attribution_charge.attribution.tutor),
+                statut=attribution_class.attribution_charge.attribution.get_function_display(),
+                responsable_de_notes=attribution_class.est_responsable_de_notes
+            )
+            if code_classe in attribution_classes_dtos:
+                attribution_classes_dtos[code_classe].append(attribution_dto)
+            else:
+                attribution_classes_dtos[code_classe] = [attribution_dto]
+
+        return attribution_classes_dtos
 
     def post(self, *args, **kwargs):
         matricule_fgs = self.request.POST.get('matricule_fgs')
@@ -128,12 +175,23 @@ class SelectScoreResponsible(LoginRequiredMixin, PermissionRequiredMixin, Templa
 
         message_bus_instance.invoke(cmd)
 
+        # for classe in self.get_attributions_classes():
+        #     matricule_fgs_classe = self.request.POST.get('matricule_fgs_' + classe)
+        #     cmd = AssignerResponsableDeNotesCommand(
+        #         code_unite_enseignement=classe,
+        #         annee_unite_enseignement=self.academic_year.year,
+        #         matricule_fgs_enseignant=matricule_fgs_classe
+        #     )
+        #
+        #     message_bus_instance.invoke(cmd)
+
         return redirect(reverse('scores_responsibles_search'))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         context['attributions'] = self.get_attributions()
+        context['attributions_classes'] = self.get_attributions_classes()
         context['code'] = self.code
         context['annee_academique'] = str(self.academic_year)
 
