1d4e9f4ec6db6b8955064005e79ee27eec8d6ac3
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2021-11-03 12:00:31 +0100

    OSIS-6200 : Fix UI issues (Issues 1 & 2)

--- assessments/views/serializers/score_sheet_xls_import.py
+++ assessments/views/serializers/score_sheet_xls_import.py
@@ -24,18 +24,17 @@
 #
 ##############################################################################
 import json
+from typing import List
 
 from django.utils.translation import gettext_lazy as _, pgettext_lazy
-
 from openpyxl.worksheet import Worksheet
 from rest_framework import serializers
 
-
 HEADER = [_('Academic year'), _('Session'), _('Learning unit'), pgettext_lazy('encoding', 'Program'),
-          _('Registration number'), _('Lastname'), _('Firstname'), _('Email'), _('Numbered scores'),
-          _('Justification (A,T)'), _('End date Prof'), _('Type of specific profile'), _('Extra time (33% generally)'),
-          _('Large print'), _('Specific room of examination'), _('Other educational facilities'),
-          _('Details other educational facilities'), _('Educational tutor'),
+          pgettext_lazy('assessments', 'Registration number'), _('Lastname'), _('Firstname'), _('Email'),
+          _('Numbered scores'), _('Justification (A,T)'), _('End date Prof'), _('Type of specific profile'),
+          _('Extra time (33% generally)'), _('Large print'), _('Specific room of examination'),
+          _('Other educational facilities'), _('Details other educational facilities'), _('Educational tutor'),
           ]
 
 
@@ -51,21 +50,25 @@ class _XLSNoteEtudiantRowImportSerializer(serializers.Serializer):
     noma = serializers.SerializerMethodField()
     row_number = serializers.SerializerMethodField()
 
-    def get_code_unite_enseignement(self, obj: tuple):
+    @staticmethod
+    def get_code_unite_enseignement(obj: tuple) -> str:
         col_unite_enseignement = HEADER.index(_('Learning unit'))
         return str(obj[col_unite_enseignement].value)
 
-    def get_note(self, obj: tuple):
+    @staticmethod
+    def get_note(obj: tuple) -> str:
         col_note = HEADER.index(_('Numbered scores'))
         raw_value = str(obj[col_note].value) if obj[col_note].value is not None else ''
         return raw_value.replace(",", ".")
 
-    def get_email(self, obj: tuple):
+    @staticmethod
+    def get_email(obj: tuple) -> str:
         col_email = HEADER.index(_('Email'))
         return str(obj[col_email].value)
 
-    def get_noma(self, obj: tuple):
-        col_noma = HEADER.index(_('Registration number'))
+    @staticmethod
+    def get_noma(obj: tuple) -> str:
+        col_noma = HEADER.index(pgettext_lazy('assessments', 'Registration number'))
         return str(obj[col_noma].value)
 
     def get_row_number(self, obj: tuple) -> int:
@@ -115,21 +118,23 @@ class ScoreSheetXLSImportSerializer(serializers.Serializer):
             )
         return academic_year_found.pop()
 
-    def __is_student_score_row(self, row) -> bool:
-        col_registration_id = HEADER.index(_('Registration number'))
+    @staticmethod
+    def __is_student_score_row(row) -> bool:
+        col_registration_id = HEADER.index(pgettext_lazy('assessments', 'Registration number'))
         raw_registration_id = row[col_registration_id].value
         return raw_registration_id and str(raw_registration_id).isdigit()
 
     def __get_student_rows(self, worksheet: Worksheet):
         return filter(self.__is_student_score_row, worksheet.rows)
 
-    def __convert_to_integer(self, raw_cell_value) -> int:
+    @staticmethod
+    def __convert_to_integer(raw_cell_value) -> int:
         try:
             return int(raw_cell_value)
         except (ValueError, TypeError,):
             return raw_cell_value
 
-    def get_notes_etudiants(self, worksheet: Worksheet):
+    def get_notes_etudiants(self, worksheet: Worksheet) -> List[_XLSNoteEtudiantRowImportSerializer]:
         notes_etudiants = []
         for row in self.__get_student_rows(worksheet):
             row_serialized = _XLSNoteEtudiantRowImportSerializer(instance=row, context={'row_number': row[0].row}).data
