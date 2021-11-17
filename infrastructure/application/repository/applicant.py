7e28172da6b7a999d32750fd91072f227d983d61
unknown <alessandro.brugnetti@uclouvain.be>
2021-11-17 10:20:43 +0100

    Remove annotate null value to zero for applicant repository

--- infrastructure/application/repository/applicant.py
+++ infrastructure/application/repository/applicant.py
@@ -116,13 +116,13 @@ def _prefetch_attributions(applicant_qs) -> List[AttributionFromRepositoryDTO]:
             )
         ),
         applicant_id_global_id=F('tutor__person__global_id'),
-        lecturing_volume_raw=Subquery(
+        lecturing_volume=Subquery(
             subqs.filter(
                 learning_component_year__type=learning_component_year_type.LECTURING
             ).values('allocation_charge')[:1],
             output_field=models.DecimalField()
         ),
-        practical_volume_raw=Subquery(
+        practical_volume=Subquery(
             subqs.filter(
                 learning_component_year__type=learning_component_year_type.PRACTICAL_EXERCISES
             ).values('allocation_charge')[:1],
@@ -132,17 +132,6 @@ def _prefetch_attributions(applicant_qs) -> List[AttributionFromRepositoryDTO]:
             When(substitute__isnull=False, then=True),
             default=False,
             output_field=models.BooleanField()
-        )
-    ).annotate(
-        lecturing_volume=Case(
-            When(lecturing_volume_raw__isnull=True, then=Value(0)),
-            default=F('lecturing_volume_raw'),
-            output_field=models.DecimalField()
-        ),
-        practical_volume=Case(
-            When(practical_volume_raw__isnull=True, then=Value(0)),
-            default=F('practical_volume_raw'),
-            output_field=models.DecimalField()
         )
     ).values(
         'course_id_code',
