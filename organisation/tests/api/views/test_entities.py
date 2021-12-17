e29fea7a4eddf22d236207939eb31c3d4b3e5bc4
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2021-12-17 14:18:28 +0100

    OS-103 : get detail of entity and add search_fields

--- organisation/tests/api/views/test_entities.py
+++ organisation/tests/api/views/test_entities.py
@@ -23,22 +23,23 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+import uuid
 
+from django.test import RequestFactory
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from base.tests.factories.entity_version import EntityVersionFactory
 from base.tests.factories.user import UserFactory
+from organisation.api.serializers.entities import EntitySerializer
 from organisation.api.views.entities import EntitiesListView
 
 
-class EntitiesListViewTestCase(APITestCase):
+class EntitesListViewTestCase(APITestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.entity_version = EntityVersionFactory(
-            entity__organization__acronym='UCL'
-        )
+        cls.entity_version = EntityVersionFactory(entity__organization__acronym='UCL')
         cls.url = reverse('organisation_api_v1:' + EntitiesListView.name, kwargs={
             'organisation_code': cls.entity_version.entity.organization.acronym
         })
@@ -80,3 +81,49 @@ class EntitiesListViewTestCase(APITestCase):
                 'end_date',
                 'logo',
             ])
+
+
+class EntityDetailTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.entity_version = EntityVersionFactory(entity__organization__acronym='UCL')
+
+        cls.user = UserFactory()
+        cls.url = reverse('organisation_api_v1:entity-detail', kwargs={
+            'organisation_code': cls.entity_version.entity.organization.acronym,
+            'uuid': cls.entity_version.uuid
+        })
+
+    def setUp(self):
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_not_authorized(self):
+        self.client.force_authenticate(user=None)
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_method_not_allowed(self):
+        methods_not_allowed = ['post', 'delete', 'put']
+
+        for method in methods_not_allowed:
+            response = getattr(self.client, method)(self.url)
+            self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_get_valid_entity(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        serializer = EntitySerializer(
+            self.entity_version,
+            context={'request': RequestFactory().get(self.url)},
+        )
+        self.assertEqual(response.data, serializer.data)
+
+    def test_get_invalid_entity_case_not_found(self):
+        invalid_url = reverse('organisation_api_v1:entity-detail', kwargs={
+            'organisation_code': self.entity_version.entity.organization.acronym,
+            'uuid': uuid.uuid4()
+        })
+        response = self.client.get(invalid_url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
