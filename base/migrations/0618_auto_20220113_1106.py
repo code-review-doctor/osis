8e5096ef43d0984847083c9f5cabb8728093eb6b
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2022-01-13 11:38:25 +0100

    OSIS-6419 : Remove from group

--- base/migrations/0618_auto_20220113_1106.py
+++ base/migrations/0618_auto_20220113_1106.py
@@ -7,6 +7,7 @@ from django.db.models import Exists, OuterRef
 def delete_tutors_without_attribution(apps, schema_editor):
     Tutor = apps.get_model('base', 'Tutor')
     AttributionNew = apps.get_model('attribution', 'AttributionNew')
+    Group = apps.get_model('auth', 'Group')
     problematic_tutors = Tutor.objects.annotate(
         has_attribution=Exists(
             queryset=AttributionNew.objects.filter(
@@ -17,6 +18,12 @@ def delete_tutors_without_attribution(apps, schema_editor):
         has_attribution=False
     )
     print("{} problematic tutors (without attributions)".format(len(problematic_tutors)))
+    group = Group.objects.get(name='tutors')
+    for tutor in problematic_tutors:
+        user = tutor.person.user
+        if user:
+            print("Removing {} from tutors group".format(tutor))
+            user.groups.remove(group)
     problematic_tutors.delete()
 
 
