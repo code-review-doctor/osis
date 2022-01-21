0c2f88a7134fea9a446108a826476b477ffc6498
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2022-01-21 14:49:33 +0100

    OSIS-6314 : DTO ProgrammeInscriptionCoursDTO

--- ddd/logic/preparation_programme_annuel_etudiant/dtos.py
+++ ddd/logic/preparation_programme_annuel_etudiant/dtos.py
@@ -69,15 +69,6 @@ class FormulaireInscriptionCoursDTO(DTO):
 
 
 @attr.s(frozen=True, slots=True, auto_attribs=True)
-class ProgrammeInscriptionCoursDTO(DTO):
-    annee_formation: int
-    sigle_formation: str
-    version_formation: str
-    intitule_complet_formation: str  # intitulé de la formation + version formation
-    racine: ContenuGroupementDTO
-
-
-@attr.s(frozen=True, slots=True, auto_attribs=True)
 class UniteEnseignementCatalogueDTO(DTO):
     bloc: int
     code: str
@@ -113,3 +104,48 @@ class FormationDTO(DTO):
     sigle: str
     version: str
     intitule_complet: str  # intitulé de la formation + version formation
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class ProgrammeInscriptionCoursDTO(DTO):
+    intitule_complet_formation: str  # intitulé de la formation + version formation
+    sous_programme = List['GroupementInscriptionCoursDTO']
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class GroupementInscriptionCoursDTO(DTO):
+    intitule_complet: str
+    obligatoire: bool
+    unites_enseignement_ajoutees: List['UniteEnseignementAjouteeDTO']
+    unites_enseignement_supprimees: List['UniteEnseignementSupprimeeDTO']
+    unites_enseignement_modifiees: List['UniteEnseignementModifieeDTO']
+    unites_enseignement: List['UniteEnseignementProgrammeDTO']
+    sous_programme = List['GroupementInscriptionCoursDTO']
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class UniteEnseignementProgrammeDTO(DTO):
+    code: str
+    intitule: str
+    obligatoire: bool
+    bloc: int
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class UniteEnseignementAjouteeDTO(DTO):
+    code: str
+    intitule: str
+    obligatoire: bool
+    bloc: int
+    a_la_suite_de: str
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class UniteEnseignementModifieeDTO(DTO):
+    code: str
+    #  TODO: champs éditables
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class UniteEnseignementSupprimeeDTO(DTO):
+    code: str
