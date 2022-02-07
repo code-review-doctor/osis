501baba4bb1a9a2ef42ab782fd3a6d5a0d231cae
mathieuzen <zen.mathieu@gmail.com>
2022-02-07 13:54:57 +0100

    OSIS-6346 WIP supprimer UE du groupement

--- ddd/logic/preparation_programme_annuel_etudiant/domain/builder/groupement_ajuste_inscription_cours_builder.py
+++ ddd/logic/preparation_programme_annuel_etudiant/domain/builder/groupement_ajuste_inscription_cours_builder.py
@@ -44,6 +44,7 @@ class GroupementAjusteInscriptionCoursBuilder(interface.RootEntityBuilder):
             repository: 'IGroupementAjusteInscriptionCoursRepository'
     ) -> 'GroupementAjusteInscriptionCours':
         return cls._build_from_group_identity(
+            code_programme=cmd.code_programme,
             group_identity=GroupIdentity(
                 code=cmd.ajouter_dans,
                 year=cmd.annee,
@@ -58,6 +59,7 @@ class GroupementAjusteInscriptionCoursBuilder(interface.RootEntityBuilder):
             repository: 'IGroupementAjusteInscriptionCoursRepository'
     ) -> 'GroupementAjusteInscriptionCours':
         return cls._build_from_group_identity(
+            code_programme=cmd.code_programme,
             group_identity=GroupIdentity(
                 code=cmd.retirer_de,
                 year=cmd.annee,
@@ -68,16 +70,17 @@ class GroupementAjusteInscriptionCoursBuilder(interface.RootEntityBuilder):
     @classmethod
     def _build_from_group_identity(
             cls,
+            code_programme: str,
             group_identity: 'GroupIdentity',
             repository: 'IGroupementAjusteInscriptionCoursRepository'
     ):
-        groupements_ajustes = repository.search(code_programme=cmd.code_programme, groupement_id=group_identity)
+        groupements_ajustes = repository.search(code_programme=code_programme, groupement_id=group_identity)
         if groupements_ajustes:
             return groupements_ajustes[0]
         return GroupementAjusteInscriptionCours(
             entity_id=IdentiteGroupementAjusteInscriptionCours(uuid=uuid.uuid4()),
             groupement_id=group_identity,
-            code_programme=cmd.code_programme,
+            code_programme=code_programme,
             unites_enseignement_ajoutees=[],
             unites_enseignement_supprimees=[],
             unites_enseignement_modifiees=[],
